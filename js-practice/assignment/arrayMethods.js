b54e0981d78db911f394473dd4a38f9be953f056
Abhimanyu <abhimanyusharma1409@gmail.com>
2024-03-27 18:01:21 +0530

    chore: arrayMethods.js file added

---
+++ js-practice/assignment/arrayMethods.js
@@ -0,0 +1,45 @@
+var myArray1 = [10, 20, 21, 30, 40, 50];
+var myArray2 = [60, 70, 80, 90, 100];
+// myArray1.splice(1,1,15,1,2)
+// myArray1.slice(1,3)
+// console.log(myArray1);
+
+var myArray3 = myArray1.concat(myArray2);
+
+console.log(myArray3)
+
+// console.log(myArray1.some((el)=>{
+//     return el>200;
+// }))
+// console.log(myArray1.filter((el)=>{
+//     return el>20;
+// }))
+// console.log(myArray1.some((el)=>{
+//     return el>200;
+// }))
+// console.log(myArray1.find((el)=>{
+//     return el>20;
+// }))
+// for(el of myArray){
+//     console.log(el);
+// }ṇ
+
+// for(i=0; i<myArray.length; i++){
+//     console.log(myArray[i]);
+// }
+
+
+// myArray.push(110);
+// myArray.pop();
+// myArray.pop();
+// console.log(myArray);
+
+// myArray1.push(55);
+// console.log(myArray1);
+// myArray1.pop();
+// console.log(myArray1);
+// var arrTemp = myArray1.concat(myArray2);
+// console.log(arrTemp);
+// console.log(myArray1.indexOf(50));
+// console.log(myArray1.reverse());
+// console.log(myArray2.slice(1, 3));
