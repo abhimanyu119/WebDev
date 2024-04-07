44454454137e0b19d4cd87c33cdbd446cab29cb6
Abhimanyu <abhimanyusharma1409@gmail.com>
2024-04-07 05:59:32 +0530

    Refactor for HTML, CSS and JavaScript files

--- dom/bookInventory/scripts.js
+++ dom/bookInventory/scripts.js
@@ -1,9 +1,36 @@
-document.addEventListener("click", function(event) {
-    if (event.target.classList.contains("book-img")) {
-        if (event.target.style.border === "10px solid rgb(200, 90, 180)") {
-            event.target.style.border = "1px solid black";
-        } else {
-            event.target.style.border = "10px solid rgb(200, 90, 180)";
+const elements = document.querySelectorAll('.sub-container');
+
+elements.forEach(element => {
+    element.addEventListener('click', function (event) {
+        this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
+    });
+});
+
+var len = document.querySelectorAll('.sub-container').length;
+document.getElementById("calculate-btn").addEventListener("click", function() {
+    var div = document.getElementsByClassName("inv-data");
+    
+    var total = 0;
+    var table = document.getElementById("inv-data-table");
+    table.innerHTML = "<tr><th>Name</th><th>Price</th><th>Total</th></tr>";
+    for(let i = 0; i < len; i++) {
+        var a = document.querySelector(".img" + (i+1) + "");
+        if (a !== null && a.parentElement.classList.contains('active')) {
+            div[0].style.visibility = "visible";
+            var bName = a.dataset.name;
+            var bPrice = a.dataset.price;
+            total += parseInt(bPrice);
+            var row = table.insertRow(-1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            cell1.innerHTML = bName;
+            cell2.innerHTML = `₹${bPrice}`;
+        }
+        else if (total === 0){
+            div[0].style.visibility = "hidden";
         }
     }
+    document.getElementById("total-price").innerHTML = `₹${total}`;
+    var totalCell = row.insertCell(2);
+    totalCell.innerHTML = `₹${total}`;
 });
