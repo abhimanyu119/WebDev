5ac947afb7ef6e3bafef29753c7de9072bb9e389
Abhimanyu <abhimanyusharma1409@gmail.com>
2024-04-08 18:07:01 +0530

    chore: added divs for horizontal scroll

--- dom/bookInventory/scripts.js
+++ dom/bookInventory/scripts.js
@@ -1,7 +1,7 @@
 const elements = document.querySelectorAll('.sub-container');
 
 elements.forEach(element => {
-    element.addEventListener('click', function (event) {
+    element.addEventListener('click', function () {
         this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
     });
 });
@@ -34,4 +34,29 @@ document.getElementById("calculate-btn").addEventListener("click", function() {
         var totalCell = row.insertCell(2);
         totalCell.innerHTML = `₹${total}`;
     }
+});
+
+var step = 300;
+
+// Get the elements with class 'preSlide' and 'nextSlide'
+var scrollleftElements = document.querySelectorAll('.scroll-left');
+var scrollrightElements = document.querySelectorAll('.scroll-right');
+
+// Function to scroll left
+function scrollLeft(event) {
+    event.preventDefault();
+    this.scrollBy(step, 0);
+}
+
+// Function to scroll right
+function scrollRight(event) {
+    event.preventDefault();
+    this.scrollBy(-step, 0);
+}
+
+document.getElementsByClassName("scroll-left").addEventListener("click", () => {
+    document.getElementById("scroll-container").scrollBy(step, 0);
+});
+document.getElementsByClassName("scroll-right").addEventListener("click", () => {
+    document.getElementById("scroll-container").scrollBy(-step, 0);
 });
