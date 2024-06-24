5a9df9ee17201b94908af5eca115852e0ef93c29
Abhimanyu <abhimanyusharma1409@gmail.com>
2024-06-24 23:42:24 +0530

    refactor: Remove unused JavaScript code and CSS styles

--- bootstrap/script.js
+++
@@ -1,8 +0,0 @@
-const num1 = 100;
-const num2 = 25.75;
-
-document.write(`The sum of ${num1} and ${num2} is ${num1 + num2}.<br><br>`);
-document.write(`The difference between ${num1} and ${num2} is ${num1 - num2}.<br><br>`);
-document.write(`The product of ${num1} and ${num2} is ${num1 * num2}.<br><br>`);
-document.write(`The quotient of ${num1} and ${num2} is ${num1 / num2}.<br><br>`);
-document.write(`The remainder of ${num1} divided by ${num2} is ${num1 % num2}`);
---
+++ js-practice/prac/script.js
@@ -0,0 +1,6 @@
+const val = parseInt(prompt("Enter a number: "));
+const val2 = parseInt(prompt("Enter another number: "));
+
+result = val + val2;
+
+document.write(`Sum: ${result}`)
